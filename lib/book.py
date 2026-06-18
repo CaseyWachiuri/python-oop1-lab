46bbf2270f3170edbc50a331c94ad3e2213976de
Casey Waweru <casey.waweru@gmail.com>
2026-06-18 10:02:07 +0300

    feat: adding a turn_page method and validation for page count

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_Count):
+        self.title = title
+        self.page_Count = page_Count
+        if (self.page_Count.isdigit()):
+            print(f"page_count must be an integer") 
+
+    def turn_page():
+        print("Flipping the page...wow, you read fast!")
