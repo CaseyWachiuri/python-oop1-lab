37c693865f3f22145fb7ace38a19dc8868d69df6
Casey Waweru <casey.waweru@gmail.com>
2026-06-18 10:29:21 +0300

    fix: Initiating book branch with validation checking changes cause I forgot to

--- lib/book.py
+++ lib/book.py
@@ -4,8 +4,12 @@ class Book:
     def __init__(self, title, page_Count):
         self.title = title
         self.page_Count = page_Count
+
+    def set_page_Count(self, value):
         if (self.page_Count.isdigit()):
+            self._page_Count = value
+        else:
             print(f"page_count must be an integer") 
 
-    def turn_page():
+    def turn_page(self):
         print("Flipping the page...wow, you read fast!")
